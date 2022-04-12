50df959a78aeaabb407169dfa899766b69636f44
Changela Vasu Dipakbhai <changelavasudipakbha@huronconsultinggroup.com>
2022-04-12 08:00:09 +0530

    day 1

---
+++ Overloading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        public static int mulDisplay(int one, int two)
+        {
+            return one * two;
+        }
+
+        public static int mulDisplay(int one, int two, int three)
+        {
+            return one * two * three;
+        }
+
+        public static int mulDisplay(int one, int two, int three, int four)
+        {
+            return one * two * three * four;
+        }
+    }
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication of two numbers: " + Program.mulDisplay(8,10));
+            Console.WriteLine("Multiplication of three numbers: " + Program.mulDisplay(10,20,30));
+            Console.WriteLine("Multiplication of four numbers: " + Program.mulDisplay(5,8,5,12));
+        }
+    }
+}
---
+++ inheritance.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Animal
+    {
+        public void Sleep()
+        {
+            Console.WriteLine("Zzzzzz");
+        }
+
+        public virtual void MakeNoise()
+        {
+            Console.WriteLine("DurrrDurrr");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("WoofWoof");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("Meaaooooow");
+        }
+    }
+
+    class test
+    {
+
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            d1.MakeNoise();
+            c1.Sleep();
+
+        }
+    }
+}
